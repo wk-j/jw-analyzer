2a5d6c4ea41bd1fda08bd3333104b51280baa62f
wk-j <somnuk.wk@gmail.com>
2018-08-09 22:08:10 +0700

    Added build script

--- tests/MyApp/Program.cs
+++ tests/MyApp/Program.cs
@@ -1,22 +1,9 @@
 ﻿using System;
 
-namespace MyApp
-{
-    internal class AAAAAA
-    {
-        int IIIII = 1000;
+namespace MyApp {
 
-        AAAAAA()
-        {
-            Console.WriteLine(IIIII);
-        }
-
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class PROGRAM {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
     }
