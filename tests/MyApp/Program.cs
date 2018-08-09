89171dad3d0e426166d6081c9c116e9084763882
wk-j <somnuk.wk@gmail.com>
2018-08-09 14:21:13 +0700

    Restructured

---
+++ tests/MyApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class AAAAAA
+    {
+        int IIIII = 1000;
+
+        AAAAAA()
+        {
+            Console.WriteLine(IIIII);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
